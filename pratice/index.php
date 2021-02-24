49ebf9b115c993c06b5a6505979d6cc9c9fa0cea
gokulksrm <54632238+gokulksrm@users.noreply.github.com>
2021-02-24 18:40:11 +0530

    Add files via upload

---
+++ pratice/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+
+
+<h1>
+<?php
+
+echo "Hello World!<br>";
+echo "this is php"."<br>";
+$model="bmw";
+$color="red";
+echo "my car model is ",$model,"<br>";
+echo "my car color is ".$color."<br>";
+
+# this is single line comment
+// this is also single line comment
+/*
+this is multi line comment
+*/
+
+
+?>
+</h1> 
+
+
+</body>
+</html>
